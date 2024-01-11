dd2cf067e3fddfe630fc98676e1904e761c03ab6
Kori Njoroge <korinjoroge63@gmail.com>
2024-01-11 23:46:27 +0300

    Variables in ruby

---
+++ variables.rb
@@ -0,0 +1,19 @@
+# Variables
+# syntax
+# Variable_name = value
+
+character_name = "Gideon"
+character_age = "22"
+
+puts ("There once was a man named " + character_name)
+puts ("He was "+ character_age + " years old")
+puts ("He really liked the name " + character_name)
+puts ("but dint likw being ") + character_age
+
+puts " "
+# Updating the value of a variable mid way
+puts ("There once was a man named " + character_name)
+puts ("He was "+ character_age + " years old")
+character_name = "Mark"
+puts ("He really liked the name " + character_name)
+puts ("but dint likw being ") + character_age

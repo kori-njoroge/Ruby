d162e89f895548b42c8df5f5c841a69d9cb519e6
Kori Njoroge <korinjoroge63@gmail.com>
2024-01-15 15:34:02 +0300

    intergers, whole and floating numbers

---
+++ Math&Numbers.rb
@@ -0,0 +1,32 @@
+puts 9 + 8
+puts 9 - 8
+puts 9 / 8
+puts 9 * 8
+puts 9%2
+
+# exponentials
+puts 2**3
+
+# with variables
+num =29
+
+puts num ** num
+
+# printing a number and string
+puts ("my favorite num is " + num.to_s)
+
+# using numbers in methods
+num2 = -45
+puts num2.abs() #gives absolute number of the passed value
+
+num3 = 45.67
+puts num3.round() #Rounds off the number.
+puts num3.ceil() #Gives upper value of a decimal
+puts num3.floor() # Gives lower value of the decimal.
+
+# Math function/method/class
+puts Math.sqrt(36) # squareroot
+puts Math.log(1) #log
+
+
+

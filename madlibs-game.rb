98b9ad06af674fd60b7cd51d37819744b1022db1
Kori Njoroge <korinjoroge63@gmail.com>
2024-01-15 16:15:21 +0300

    madlibs game in Ruby using user input

---
+++ madlibs-game.rb
@@ -0,0 +1,11 @@
+puts "Enter Color :"
+color = gets.chomp().downcase()
+puts "Enter a Plural noun: "
+plural_noun = gets.chomp().capitalize
+puts "Enter Celebrity name: "
+celebrity = gets.chomp().capitalize
+
+
+puts ("Roses are "+ color)
+puts (plural_noun+ " are blue")
+puts ("I love " + celebrity)

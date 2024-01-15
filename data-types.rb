59531cfd7252959a20e5b8314b559ffcb4b2f0ed
Kori Njoroge <korinjoroge63@gmail.com>
2024-01-15 15:08:37 +0300

    String data types in ruby.

--- data-types.rb
+++ data-types.rb
@@ -3,3 +3,13 @@ age = 75 #number
 gpa = 3.22 #floating point numbers
 is_male = true #boolean
 flaws = nil #has no value
+
+
+# STRINGS IN RUBY.
+puts "I'm awesome"
+# escaping in ruby
+puts "I'm \"awesoooome"
+
+
+phrase = "I'm awesome"
+puts phrase

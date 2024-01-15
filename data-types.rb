955bb21db2f251e41eb418fd935a8aea7fbebcf5
Kori Njoroge <korinjoroge63@gmail.com>
2024-01-15 15:20:40 +0300

    String methods in Ruby

--- data-types.rb
+++ data-types.rb
@@ -13,3 +13,18 @@ puts "I'm \"awesoooome"
 
 phrase = "I'm awesome"
 puts phrase
+
+# STRING METHODS
+puts phrase.upcase() #converts everything to Uppercase letter
+puts phrase.downcase() #converts everything to Uppercase letter
+puts phrase.strip() #Removes uneccesary spaces , infront and after
+puts phrase.length() #Length of the string phrase-characters
+puts phrase.include? "o" # returns true or false > check whether the string is present in the text
+puts phrase[10] #prints the charcter in the position entered in the square bracket.
+puts phrase[0,2] #prints a range of charcters in the provided index range. > minus the charcter in the specified upperbound.
+puts phrase.index("o") #prints the index postition of the specified character in the string.
+
+# Some applications without variables
+puts "programming".upcase()
+puts "programming".length()
+puts "programming"[3]
